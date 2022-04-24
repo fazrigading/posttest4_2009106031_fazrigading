4041ba3e4943ec5f8f5f943b08a6d1972bbce71f
fazrigading <super.fai700@gmail.com>
2022-04-24 23:02:42 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:posttest4_2009106031_fazrigading/landingpage.dart';
 import 'button.dart';
-import 'mainpage.dart';
 import 'nftlist.dart';
 import 'profilepage.dart';
 import 'landingpage.dart';
@@ -176,5 +175,32 @@ class _MainPageState extends State<MainPage> {
         },
       ),
     );
+  }
+}
+
+class SubmenuTitles extends StatelessWidget {
+  const SubmenuTitles({Key? key, required this.teks}) : super(key: key);
+  final String teks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 5, left: 20, right: 20),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            teks,
+            style: const TextStyle(
+              color: Color.fromRGBO(18, 18, 18, 1),
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          TextButton(
+              onPressed: () {},
+              child: const Text('View all'),
+              style: TextButton.styleFrom(
+                  primary: const Color.fromRGBO(132, 132, 132, 1)))
+        ]));
   }
 }
